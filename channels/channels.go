4aee69b427ae6d94cd0314b1a9b480f635855835
Clark Ritchie <clarkritchie@gmail.com>
2024-10-03 08:47:03 -0700

    cleanup from yesteryear

--- channels/channels.go
+++ channels/channels.go
@@ -1,20 +1,36 @@
 package main
 
-// Simple example of using channels
+// Simple example of using ch1annels
 
 import (
 	"fmt"
 	"time"
 )
 
-func main() {
-	ch := make(chan string)
-	go fooBar(ch)
-	msg := <-ch
-	fmt.Printf("message:  %v", msg)
+func bar(ch chan string) {
+	for i := 0; i < 5; i++ {
+		ch <- fmt.Sprintf("bar %v", i)
+	}
+	close(ch)
 }
 
-func fooBar(ch chan<- string) {
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go foo(ch1)
+	go bar(ch2)
+	go bar(ch1)
+
+	msg1 := <-ch1
+	fmt.Printf("message 1: %v\n", msg1)
+
+	for msg2 := range ch2 {
+		fmt.Printf("message 2: %v\n", msg2)
+	}
+}
+
+func foo(ch chan<- string) {
 	time.Sleep(2 * time.Second)
-	ch <- "hello world"
+	ch <- "foo"
 }
